60749c0eeb8bfa56353eced0d6b82a1fd37443e7
mino3377 <nwawuba1020yuma@gmail.com>
2026-03-03 12:12:06 +0900

    delete NewProject and NewMaterial

--- src/app/(private)/new-project/page.tsx
+++
@@ -1,7 +0,0 @@
-// C:\Users\chiso\nextjs\study-allot\src\app\(private)\add-project\page.tsx
-import AddProjectForm from "@/app/(private)/new-project/page-body"
-import { createProject } from "./action"
-
-export default function AddProjectPage() {
-  return <AddProjectForm onSubmit={createProject} />
-}
